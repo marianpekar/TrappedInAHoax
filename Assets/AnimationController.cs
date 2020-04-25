9de2386a9efe3d9496c1d35735bdd54e84e86a84
Marian Pekár <marian.pekar@gmail.com>
2020-04-25 12:09:05 +0200

    Initial commit

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimationController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void FixedUpdate()
+    {
+        animator.SetBool("isRunning", !(agent.remainingDistance <= 0.1));
+    }
+}
---
+++ Assets/Source/Players/PlayerMovementManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovementManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject selectedPlayer;
+    
+    private NavMeshAgent agent; 
+
+    void Start()
+    {
+        SetComponents(selectedPlayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hit, 1000)) return;
+
+        if (hit.collider.gameObject.CompareTag("Player"))
+            SelectPlayer(hit.collider.gameObject);
+        else
+            SetTarget(hit.point);
+    }
+
+    void SetComponents(GameObject player)
+    {
+        agent = player.GetComponent<NavMeshAgent>();
+    }
+
+    void SelectPlayer(GameObject player)
+    {
+        SetComponents(player);
+    }
+
+    void SetTarget(Vector3 target)
+    {
+        agent.destination = target;
+    }
+}
