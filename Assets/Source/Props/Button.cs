e9770934524d8857cfa9d3a9e06f475224904dc5
Marian Pekár <marian.pekar@gmail.com>
2020-04-26 00:16:28 +0200

    Add button with UnityEvents

---
+++ Assets/Source/Props/Button.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button : MonoBehaviour
+{
+    public UnityEvent OnButtonPressed;
+    public UnityEvent OnButtonReleased;
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision otheCollider)
+    {
+        if (otheCollider.gameObject.CompareTag("Pickable"))
+        {
+            OnButtonPressed.Invoke();
+        }
+    }
+
+    void OnCollisionExit(Collision otheCollider)
+    {
+        if (otheCollider.gameObject.CompareTag("Pickable"))
+        {
+            OnButtonReleased.Invoke();
+        }
+    }
+}
