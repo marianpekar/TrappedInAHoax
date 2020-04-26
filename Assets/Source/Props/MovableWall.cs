8b11e49863cc61f7ffe31edca6bc223e78dedfeb
Marian Pekár <marian.pekar@gmail.com>
2020-04-26 09:05:41 +0200

    Add movable wall

---
+++ Assets/Source/Props/MovableWall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableWall : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 targetPosition;
+
+    [SerializeField]
+    private float speed;
+
+    private Vector3 defaultPosition;
+    private bool isMovingToTargetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMovingToTargetPosition)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, defaultPosition, Time.deltaTime * speed);
+        }
+    }
+
+    public void MoveToTargetPosition()
+    {
+        isMovingToTargetPosition = true;
+    }
+
+    public void MoveToDefaultPosition()
+    {
+        isMovingToTargetPosition = false;
+    }
+}
