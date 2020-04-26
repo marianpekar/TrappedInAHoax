e8f86b5848f04abb0cad0c8d7877e454e3db0b4a
Marian Pekár <marian.pekar@gmail.com>
2020-04-26 17:49:15 +0200

    Make button glow when pressed

--- Assets/Source/Props/Button.cs
+++ Assets/Source/Props/Button.cs
@@ -9,11 +9,25 @@ public class Button : MonoBehaviour
     public UnityEvent OnButtonPressed;
     public UnityEvent OnButtonReleased;
 
+    [SerializeField]
+    private Material pressedMaterial;
+
+    [SerializeField]
+    private Material releasedMaterial;
+
+    private Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
     // Update is called once per frame
     void OnTriggerStay(Collider otheCollider)
     {
         if (otheCollider.gameObject.CompareTag("Pickable") || otheCollider.gameObject.CompareTag("Player"))
         {
+            SetMaterial(pressedMaterial);
             OnButtonPressed.Invoke();
         }
     }
@@ -22,7 +36,15 @@ public class Button : MonoBehaviour
     {
         if (otheCollider.gameObject.CompareTag("Pickable") || otheCollider.gameObject.CompareTag("Player"))
         {
+            SetMaterial(releasedMaterial);
             OnButtonReleased.Invoke();
         }
+    }
+
+    void SetMaterial(Material material)
+    {
+        var materials = renderer.materials;
+        materials[1] = material;
+        renderer.materials = materials;
     }
 }
